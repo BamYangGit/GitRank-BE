53582a70fff83f60d390db2a4178c5f2ae2a5438
HongSJae <gtw030488@gmail.com>
2024-03-31 23:01:00 +0900

    🎉 :: Project Setting

---
+++ GitRank-BE/src/main/kotlin/com/bamyanggit/gitrankbe/GitRankBeApplication.kt
@@ -0,0 +1,11 @@
+package com.bamyanggit.gitrankbe
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GitRankBeApplication
+
+fun main(args: Array<String>) {
+    runApplication<GitRankBeApplication>(*args)
+}
---
+++ GitRank-BE/src/test/kotlin/com/bamyanggit/gitrankbe/GitRankBeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bamyanggit.gitrankbe
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GitRankBeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
